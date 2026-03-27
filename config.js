966a15d091e232abed354cbbcabd7b0802c0f5d5
dineshsatelkar <56499538+dineshsatelkar-11@users.noreply.github.com>
2026-03-27 16:29:55 +0530

    Update bakery location coordinates in config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 
 const CONFIG = {
   API_URL: 'https://script.google.com/macros/s/AKfycbybQWGn9VUWrWPy7FaYRLcuRDyRZx-LL6v3e-SXFF7qv5Xm7oD8xUt4Gp_XDCZ02QguEg/exec', // ← paste after deploy
-  BAKERY: { lat: 18.5820, lng: 73.9197, name: 'Baked HQ · Lohegaon' },
+  BAKERY: { lat: 18.607176075062227, lng: 73.9273947804779, name: 'Baked HQ · Lohegaon' },
   DRIVERS: ['Bharat','Anand','Vikas','Yuvraj','Rama'],
   ADMIN_PASS: 'baked@admin',
   VERSION: '1.0'
