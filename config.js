cd1b3c0bda92ef1c664512378d13bd04db6ba459
dineshsatelkar <56499538+dineshsatelkar-11@users.noreply.github.com>
2026-03-29 20:40:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // ─────────────────────────────────────────────
 
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbybQWGn9VUWrWPy7FaYRLcuRDyRZx-LL6v3e-SXFF7qv5Xm7oD8xUt4Gp_XDCZ02QguEg/exec', // ← paste after deploy
+  API_URL: 'https://script.google.com/macros/s/AKfycbyi-IJ6NLR28hRTeesy52THTbdcYCSnz3lp7NtEaJsxBOiNOm-9emMu0xOHT6hvWSE-/exec', // ← paste after deploy
   BAKERY: { lat: 18.607176075062227, lng: 73.9273947804779, name: 'Baked HQ · Lohegaon' },
   DRIVERS: ['Bharat','Anand','Vikas','Yuvraj','Rama'],
   ADMIN_PASS: 'baked@admin',
